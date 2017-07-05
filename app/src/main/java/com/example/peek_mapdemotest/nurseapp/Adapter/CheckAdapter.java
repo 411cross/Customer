820a774bc304c69f081de30e3fbf9386725b4c61
嘉浚浚浚浚 <976207331@qq.com>
2017-07-05 12:37:51 +0800

    checkOrdering

--- app/src/main/java/com/example/peek_mapdemotest/nurseapp/Adapter/CheckAdapter.java
+++ app/src/main/java/com/example/peek_mapdemotest/nurseapp/Adapter/CheckAdapter.java
@@ -7,7 +7,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import com.example.peek_mapdemotest.nurseapp.Activity.textorders;
+import com.example.peek_mapdemotest.nurseapp.Entity.Order;
 import com.example.peek_mapdemotest.nurseapp.R;
 
 import java.util.List;
@@ -16,9 +16,9 @@ import java.util.List;
  * Created by DEMON on 2017/6/28.
  */
 
-public class CheckAdapter extends ArrayAdapter<textorders> {
+public class CheckAdapter extends ArrayAdapter<Order> {
     private int resource;
-    public CheckAdapter(Context context,int resourceID,List<textorders> objects)
+    public CheckAdapter(Context context,int resourceID,List<Order> objects)
     {
         super(context,resourceID,objects);
                 resource=resourceID;
@@ -26,17 +26,17 @@ public class CheckAdapter extends ArrayAdapter<textorders> {
     @Override
     public View getView(int position,View converView,ViewGroup parent)
     {
-        textorders to=getItem(position);
+        Order to=getItem(position);
         View view = LayoutInflater.from(getContext()).inflate(resource,parent,false);
         TextView textid=(TextView)view.findViewById(R.id.textViewId);
         TextView textmoney =(TextView)view.findViewById(R.id.textViewMoney);
         TextView textdate=(TextView)view.findViewById(R.id.textViewDate);
         TextView textstatus=(TextView)view.findViewById(R.id.textViewStatus);
       //  ImageView image=(ImageView) view.findViewById(R.id.imageView1);
-        textid.setText(to.getID());
-        textmoney.setText(to.getMoney());
-        textdate.setText(to.getDate());
-        textstatus.setText(to.getStatus());
+//        textid.setText(to.getID());
+//        textmoney.setText(to.getMoney());
+//        textdate.setText(to.getDate());
+//        textstatus.setText(to.getStatus());
         return view;
     }
 }
